9a75107f324ce1ecca33038845d39f18582883a5
Da Kim <daeunkim127@gmail.com>
2022-07-16 10:00:19 -0400

    client file debugged

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,16 +1,17 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
+
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log("PUT to the database");
@@ -35,6 +36,5 @@ export const getDb = async () => {
     : console.log("🚀 - data not found in the database");
   return result?.value;
 };
-
 
 initdb();
